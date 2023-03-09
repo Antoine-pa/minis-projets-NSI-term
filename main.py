ccfd343b21f814aae382eda8ed3e481e1bada71b
Antoine-pa <68688365+Antoine-pa@users.noreply.github.com>
2023-03-09 10:30:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,100 +1,134 @@
+
 import pygame
 import time
+import sys
+import os
+from math import sin,cos
+SIZE_TEXT = 20
+BLACK = (0, 0, 0)
+RED = (150, 0, 0)
+WHITE = (255, 230, 240)
+GREY_WHITE = (200, 200, 200)
+GREY = (100, 100, 100)
+SENSIBILITY = 300
+VITESSE_ENEMY = 1
 
 pygame.init()
 screen_info = pygame.display.Info()
 screen = pygame.display.set_mode((screen_info.current_w,screen_info.current_h))
-size_x = screen_info.current_w
-size_y = screen_info.current_h
+
+NB_COL = 25
+NB_LINE = int(NB_COL*(screen_info.current_h/screen_info.current_w))
+LIFE = 2
+SIZE_PERSO=10
+
+coord_sous_fenetre = [0, 0]
+liste_enemies = []
+direction_point = [100, 0]
+def load_ground(x, y):
+    ground = pygame.image.load('sol.png')
+    ground = pygame.transform.scale(ground,(x, y))
+    return ground
+
+ground = load_ground(screen_info.current_w//NB_COL, screen_info.current_h//NB_LINE)
 
 
-BLACK = (0, 0, 0)
-RED = (150, 0, 0)
-WHITE = (255, 255, 255)
-GREY_WHITE = (200, 200, 200)
-GREY = (100, 100, 100)
-SENSIBILITY = 0.4
-MENU_EDIT_POS = (5*size_x//6, size_y//2, size_x//6, size_y//2)
-LONG_COL_MENU_EDIT = MENU_EDIT_POS[2] // 3
-LONG_BLOCK_MENU_EDIT = 2*LONG_COL_MENU_EDIT//3
-GAP_BLOCK_COL_MENU_EDIT = LONG_COL_MENU_EDIT//6
-LIST_BAT_MENU_EDIT = ["Caserne1", "Caserne1", "Caserne1"]
-ZOOM = 1
-
-
-def load_img(name, x, y):
-    img = pygame.image.load(name)
-    img = pygame.transform.scale(img,(x, y))
-    return img
-
-
-class Map:
+class FPS:
     def __init__(self):
-        self.x = 1000
-        self.y = 1000
-        self.pos = [self.x/2, self.y/2]
-        self.list_bat = [Caserne(503, 506)]
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.Font("Melon Honey.ttf", 20)
+        self.text = self.font.render(str(self.clock.get_fps()), True, BLACK)
+
+    def render(self, display):
+        self.text = self.font.render(str(round(self.clock.get_fps(),2)), True, BLACK)
+        display.blit(self.text, (20, 20))
+
+#fps = FPS()
+
+class Vect2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def norme(self):
+        return (self.x**2+self.y**2)**0.5
+
+    def normalize(self):
+        n = self.norme()
+        self.x /= n
+        self.y /= n
+        return self
+    def __repr__(self):
+        return f"({self.x}, {self.y})"
+
+    def __mult__(self, other):
+        if isinstance(other, int):
+            self.x *= other
+            self.y *= other
+            return self
+        elif isinstance(other, Vect2):
+            return self.x*other.x+self.y*other.y
+        else:
+            return None
+
+class Enemie:
+    def __init__(self, x, y):
+        self.life = 1
+        self.size = 10
+        self.coords = [x, y]
+        liste_enemies.append(self)
+
+    def __repr__(self):
+        return f"life : {self.life}\npos : {self.coords}\n"
+
+    def update(self):
+        v = Vect2(screen_info.current_h//2+coord_sous_fenetre[0]-self.coords[0], screen_info.current_w//2+coord_sous_fenetre[1]-self.coords[1]).normalize()
+        self.coords[0] += v.x*VITESSE_ENEMY
+        self.coords[1] += v.y*VITESSE_ENEMY
+        dist = round(((screen_info.current_h//2+coord_sous_fenetre[0]-self.coords[0])**2+(screen_info.current_w//2+coord_sous_fenetre[1]-self.coords[1])**2)**0.5)
+        t=[None, dist]
+        if ((self.coords[0]-(coord_sous_fenetre[0]+screen_info.current_h//2))**2+(self.coords[1]-(coord_sous_fenetre[1]+screen_info.current_w//2))**2)**0.5 < self.size+SIZE_PERSO:
+            t[0] = time.time()
+
+        return t
+
+    def shoot(self, deg):
+        self.life -= deg
+        if self.life <= 0:
+            return True
+        return False
 
     def display(self):
-        screen.fill(WHITE)
-        for x in range(0, size_x+50, 50):
-            pygame.draw.line(screen, BLACK, (x-self.pos[0]%1*50, 0), (x-self.pos[0]%1*50, size_y))
-            for y in range(0, size_y+50, 50):
-                pygame.draw.line(screen, BLACK, (0, y-self.pos[1]%1*50), (size_x, y-self.pos[1]%1*50))
-        for b in self.list_bat:
-            b.display(*self.pos)
+        pygame.draw.circle(screen, RED, (self.coords[1]-coord_sous_fenetre[1], self.coords[0]-coord_sous_fenetre[0]), self.size)
 
-class Menu:
-    def __init__(self):
-        self.action = "map"
-        self.mem_tamp = None
-    
-    def click(self, act):
-        if self.action != act:
-            if act == "edit":
-                self.mem_tamp = [None, None]
-            else:
-                self.mem_tamp = None
-            self.action = act
-        else:
-            self.action = "map"
-            self.mem_tamp = None
-
-    
-    def display(self, screen, ressources):
-        if self.action == "map":
-            pass 
-        elif self.action == "edit":
-            pygame.draw.rect(screen, GREY_WHITE, pygame.Rect(*MENU_EDIT_POS), 0)
-            for i in range(len(LIST_BAT_MENU_EDIT)):
-                img = load_img(LIST_BAT_MENU_EDIT[i]+".png", LONG_BLOCK_MENU_EDIT, LONG_BLOCK_MENU_EDIT)
-                pygame.draw.line(screen, BLACK, (MENU_EDIT_POS[0] + (i+1)*LONG_COL_MENU_EDIT, MENU_EDIT_POS[1]), (MENU_EDIT_POS[0]+ (i+1)*LONG_COL_MENU_EDIT, size_y))
-                screen.blit(img, (MENU_EDIT_POS[0] + i*LONG_COL_MENU_EDIT + GAP_BLOCK_COL_MENU_EDIT, MENU_EDIT_POS[1] + GAP_BLOCK_COL_MENU_EDIT))
-        elif self.action == "settings":
-            self.display_settings(screen)
-        elif self.action == "ressources":
-            self.display_ressources(screen, ressources)
+def dalle(*coord):
+    #dalle(i+1, j, len_col, len_line)
+    GRIS = [127, 127, 127]
+    #pygame.draw.rect(screen, GRIS, coord)
+    n = 3
+    for y in range(n):
+        col = [i+int(round(((cos(sin(y)*50))+1)*25)) for i in GRIS]
+        #print(col)
+        pygame.draw.rect(screen, col, (coord[0], coord[1]+y*coord[2]//n, coord[2], coord[3]))
 
 
-    def display_ressources(self, screen, ressources):
-        screen.fill(BLACK)
-        pygame.draw.rect(screen, GREY_WHITE, pygame.Rect(size_x//4, size_y//4, size_x//2, size_y//2), 0)
-        ressources = list(ressources.items())
-        for i in range(len(ressources)):
-            res = ressources[i]
-            ratio = res[1][1] / res[1][0]
-            barre((size_x/2-size_x/16, size_y*(3*len(ressources)+2+6*i)/(12*len(ressources))), (size_x/8, size_y/(6*len(ressources))), ratio, RED)
-            Text(res[0], BLACK, (size_x/4+size_x/16, size_y/4+size_y/(6*len(ressources))+i*size_y/(2*len(ressources))), int(size_y/(6*len(ressources))))
-            Text(str(res[1][0])+"/"+str(res[1][1])+" ("+str(round(ratio*100, 2))+"%)", BLACK, (size_x/2+size_x/8, size_y/4+size_y/(6*len(ressources))+i*size_y/(2*len(ressources))), int(size_y/(6*len(ressources))))
+def title_box(box:tuple, title:str) -> None:
+    pos = (box[0], box[1]-SIZE_TEXT-2)
+    Text(title, BLACK, pos, SIZE_TEXT)
 
-    def display_settings(self, screen):
-        screen.fill(BLACK)
-        pygame.draw.rect(screen, GREY_WHITE, pygame.Rect(size_x//4, size_y//4, size_x//2, size_y//2), 0)
- 
-def barre(pos : tuple, size : tuple, ratio : float, color : tuple):
-    pygame.draw.rect(screen, color, pygame.Rect(pos[0], pos[1], size[0]*ratio, size[1]), 0)
-    pygame.draw.rect(screen, BLACK, pygame.Rect(pos[0], pos[1], size[0], size[1]), 1)
+def pos_text(box:tuple, thickness_box:int) -> tuple:
+    return (box[0]+thickness_box+3, box[1]+box[3]-thickness_box-SIZE_TEXT)
 
+def Box(coord:tuple, thickness:int, text:str, title=None):
+    """
+    fonction qui permet de créer une box dans le menu
+    """
+    box = pygame.Rect(*coord)
+    pygame.draw.rect(screen, BLACK, box, thickness)
+    Text(text, BLACK, pos_text(coord, thickness), SIZE_TEXT)
+    if title is not None:
+        title_box(coord, title)
+    return box
 
 def Text(text, color, pos: tuple, size):
     """
@@ -102,108 +136,162 @@ def Text(text, color, pos: tuple, size):
     """
     FONT = pygame.font.Font("Melon Honey.ttf", size)
     screen.blit(FONT.render(text, True, color), pos)
+    pygame.display.update()
     del FONT
 
+def update_enemies():
+    touche = False
+    _t = None
+    list_dist = []
+    list_en_dist = []
+    kill = False
+    for en in range(len(liste_enemies)):
+        if liste_enemies[en-int(kill)].life <= 0:
+            del liste_enemies[en-int(kill)]
+            kill = True
+        else:
+            t = liste_enemies[en-int(kill)].update()
+            list_dist.append(t[1])
+            list_en_dist.append(liste_enemies[en-int(kill)])
+            if not touche and t[0] is not None:
+                touche = True
+                _t = t[0]
+            liste_enemies[en-int(kill)].display()
+    if list_dist != []:
+        en = min(list_dist)
+        en = [en, list_en_dist[list_dist.index(en)]]
+    else:
+        en = [None, None]
+    return [_t, en]
 
-class UsineCanon:
-    def __init__(self):
-        self.list_bat = []
-        self.max = 2
-        self.file = ""
+def update_play():
+    screen.fill(BLACK)
+    len_col = screen_info.current_w//NB_COL
+    len_line = screen_info.current_h//NB_LINE
 
-    def construire(self):
-        pass
+    for i in range(-int(round(coord_sous_fenetre[1]))%len_line-len_col, screen_info.current_w-int(round(coord_sous_fenetre[1]))%len_line+len_col, len_col):
+        if coord_sous_fenetre[0] < 0:
+            pygame.draw.line(screen, (81, 47, 32), (i, coord_sous_fenetre[0]), (i, screen_info.current_h-coord_sous_fenetre[0]))
+        else:
+            pygame.draw.line(screen, (81, 47, 32), (i, -coord_sous_fenetre[0]), (i, screen_info.current_h+coord_sous_fenetre[0]))
+        for j in range(-int(round(coord_sous_fenetre[0]))%len_col-len_line, screen_info.current_h-int(round(coord_sous_fenetre[0]))%len_col+len_line, len_line):
+            if coord_sous_fenetre[1] < 0:
+                pygame.draw.line(screen, (81, 47, 32), (coord_sous_fenetre[1], j), (screen_info.current_w-coord_sous_fenetre[1], j))
+            else:
+                pygame.draw.line(screen, (81, 47, 32), (-coord_sous_fenetre[1], j), (screen_info.current_w+coord_sous_fenetre[1], j))
+            screen.blit(ground, (i+1, j))
+    #fps.render(screen)
+    pygame.draw.circle(screen, WHITE, (screen_info.current_w//2, screen_info.current_h//2), SIZE_PERSO)
+    t = update_enemies()
+    #pygame.display.update()
+    if direction_point is not None:
+        #cible = pygame.image.load('cible.png')
+        #cible = pygame.transform.scale(cible,(51, 51))
+        #screen.blit(ground, (direction_point[1]-coord_sous_fenetre[1]-25, direction_point[0]-coord_sous_fenetre[0]-25))
+        pygame.draw.circle(screen, (0, 255, 0), (direction_point[1]-coord_sous_fenetre[1], direction_point[0]-coord_sous_fenetre[0]), 10)
 
-class Canon:
-    def __init__(self):
-        self.type = ""
-        self.deg = 0
+    pygame.display.update()
+    #fps.clock.tick(60)
+    return t
+
+def display_menu():
+    bg = pygame.image.load('bg.png')
+    bg = pygame.transform.scale(bg,(screen_info.current_w, screen_info.current_h))
+    screen.blit(bg, (0, 0))
+    pygame.display.update()
+    button_play = Box((20, 20, 60, 30), 2, "jouer")
+    return button_play
+
+def update_pos(direction_point, last_frame):
+    diff_frame = time.time() - last_frame
+    dep = SENSIBILITY*diff_frame
+    if direction_point is not None:
+        v = Vect2(direction_point[0]-(screen_info.current_h//2+coord_sous_fenetre[0]), direction_point[1]-(screen_info.current_w//2+coord_sous_fenetre[1])).normalize()
+        coord_sous_fenetre[0] += v.x*dep
+        coord_sous_fenetre[1] += v.y*dep
+        if ((direction_point[0]-(screen_info.current_h//2+coord_sous_fenetre[0]))**2+(direction_point[1]-(screen_info.current_w//2+coord_sous_fenetre[1]))**2)**0.5 < 10:
+            direction_point = None
+    return coord_sous_fenetre, direction_point
 
 
-class Caserne:
-    def __init__(self, x, y):
-        self.list_unit = []
-        self.max = 5
-        self.pos = [x, y]
-        self.img = load_img("Caserne1.png", 100, 100)
+def reset_enemy():
+    for en in liste_enemies:
+        del en
+    Enemie(20, 20)
+    Enemie(50, 50)
 
-    def former(self):
-        pass
-
-    def display(self, x, y):
-        if x-2 <=self.pos[0] <= size_x/50+x and y-2 <=self.pos[1] <= size_y/50+y:
-            screen.blit(self.img, ((self.pos[0]-x)*50, (self.pos[1]-y)*50))
+def tire(dist, en, deg):
+    en.shoot(deg)
 
 
-class Game:
-    def __init__(self):
-        self._map = Map()
-        self._menu = Menu()
-        self.ressources = {"bois" : (100, 75), "fer" : (200, 60), "eau" : (100, 60), "feu" : (60, 40), "acier" : (200, 30), "or" : (60, 12), "charbon" : (120, 79)}
-    
-    def display(self, screen):
-        if self._menu.action in ("map", "edit"):
-            self._map.display()
-        self._menu.display(screen, self.ressources)
-        pygame.display.update()
-
-    def deplacement(self, x, y):
-        self._map.pos[0] += x
-        self._map.pos[1] += y
-
-game = Game()
 continuer = True
+button_play = display_menu()
+play = False
+reset_enemy()
+t_last_att = time.time()
+last_frame = time.time()
 while continuer:
-    game.display(screen)
+    #Evénements
+    if play:
+        coord_sous_fenetre, direction_point = update_pos(direction_point, last_frame)
+        last_frame = time.time()
+        t = update_play()
+        if t[0] is not None:
+            if t[0]-t_last_att > 5:
+                t_last_att=t[0]
+                LIFE -=1
+                if LIFE == 0:
+                    play = False
+                    coord_sous_fenetre = [0, 0]
+                    direction_point = None
+                    button_play = display_menu()
+        dist = t[1]
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            k = pygame.key.get_pressed()
-            if k[pygame.K_F1]:
-                game._menu.click("settings")
-            if k[pygame.K_r]:
-                game._menu.click("ressources")
-            if k[pygame.K_e]:
-                game._menu.click("edit")
 
-            if game._menu.action in ("map", "edit"):
-                """
-                if k[pygame.K_z] and k[pygame.K_d]:
-                    coord_sous_fenetre[0] -= int((2**0.5*SENSIBILITY)/2)
-                    coord_sous_fenetre[1] += int((2**0.5*SENSIBILITY)/2)
-                elif k[pygame.K_z] and k[pygame.K_q]:
-                    coord_sous_fenetre[0] -= int((2**0.5*SENSIBILITY)/2)
-                    coord_sous_fenetre[1] -= int((2**0.5*SENSIBILITY)/2)
-                elif k[pygame.K_q] and k[pygame.K_s]:
-                    coord_sous_fenetre[0] += int((2**0.5*SENSIBILITY)/2)
-                    coord_sous_fenetre[1] -= int((2**0.5*SENSIBILITY)/2)
-                elif k[pygame.K_s] and k[pygame.K_d]:
-                    coord_sous_fenetre[0] += int((2**0.5*SENSIBILITY)/2)
-                    coord_sous_fenetre[1] += int((2**0.5*SENSIBILITY)/2)
-                """
-                if k[pygame.K_d]:
-                    game._map.pos[0] += SENSIBILITY
-                elif k[pygame.K_s]:
-                    game._map.pos[1] += SENSIBILITY
-                elif k[pygame.K_z]:
-                    game._map.pos[1] -= SENSIBILITY
-                elif k[pygame.K_q]:
-                    game._map.pos[0] -= SENSIBILITY
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if game._menu.action == "edit":
-                pos = list(pygame.mouse.get_pos())
-                if MENU_EDIT_POS[0] < pos[0] and MENU_EDIT_POS[1] < pos[1]:
-                    pos[0], pos[1] = pos[0] - MENU_EDIT_POS[0], pos[1] - MENU_EDIT_POS[1]
-                    coord_case = (int(pos[0]/MENU_EDIT_POS[2]*3), 0)
-                    index = 3*coord_case[1] + coord_case[0]
-                    bat = LIST_BAT_MENU_EDIT[index]
-                    
-
-
+        #Quitter
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 continuer = False
+        if event.type == pygame.KEYDOWN:
+            if play:
+                k = pygame.key.get_pressed()
+                if k[pygame.K_e]:
+                    if dist[1] is not None:
+                        tire(*dist, 1)
+            """
+            k = pygame.key.get_pressed()
+            if k[pygame.K_z] and k[pygame.K_d]:
+                coord_sous_fenetre[0] -= int((2**0.5*SENSIBILITY)/2)
+                coord_sous_fenetre[1] += int((2**0.5*SENSIBILITY)/2)
+            elif k[pygame.K_z] and k[pygame.K_q]:
+                coord_sous_fenetre[0] -= int((2**0.5*SENSIBILITY)/2)
+                coord_sous_fenetre[1] -= int((2**0.5*SENSIBILITY)/2)
+            elif k[pygame.K_q] and k[pygame.K_s]:
+                coord_sous_fenetre[0] += int((2**0.5*SENSIBILITY)/2)
+                coord_sous_fenetre[1] -= int((2**0.5*SENSIBILITY)/2)
+            elif k[pygame.K_s] and k[pygame.K_d]:
+                coord_sous_fenetre[0] += int((2**0.5*SENSIBILITY)/2)
+                coord_sous_fenetre[1] += int((2**0.5*SENSIBILITY)/2)
+            elif k[pygame.K_d]:
+                coord_sous_fenetre[1] += SENSIBILITY
+            elif k[pygame.K_s]:
+                coord_sous_fenetre[0] += SENSIBILITY
+            elif k[pygame.K_z]:
+                coord_sous_fenetre[0] -= SENSIBILITY
+            elif k[pygame.K_q]:
+                coord_sous_fenetre[1] -= SENSIBILITY
+            """
         if event.type == pygame.QUIT:
             pygame.quit()
             continuer = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            #colonnes
+            if not play and button_play.collidepoint(event.pos):
+                del button_play
+                play= True
+            elif play:
+                direction_point = list(pygame.mouse.get_pos())[::-1]
+                direction_point[0] += coord_sous_fenetre[0]
+                direction_point[1] += coord_sous_fenetre[1]
